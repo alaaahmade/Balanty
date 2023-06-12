268d4369bce57ed9ee89baca5593e63082985cbc
alaaahmade <alaaahmade66@gmail.com>
2023-06-12 14:07:06 +0300

    refactor: remove unusable file

--- server/config/connection.ts
+++
@@ -1,5 +0,0 @@
-import { Sequelize } from 'sequelize';
-import 'dotenv/config';
-import { dbUrl, sequelizeOption } from './environment';
-
-export const sequelize = new Sequelize(dbUrl, sequelizeOption);
