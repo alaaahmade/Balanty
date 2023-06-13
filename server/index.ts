61e5a2ae6f7ad2cf081c8f99dcaee116a0138044
Emanalabsi <emanabsi30@gmail.com>
2023-06-13 14:10:07 +0300

    fix: return deleted content for index in server

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,16 @@
-export {};
+import app from './app';
+import { PORT } from './config';
+import { sequelize } from './database';
+const startServer = async (): Promise<void> => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    app.listen(PORT, (): void => {
+      console.log(`server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+startServer();
