7acd5fe6d2c32675fb96512a4b6dc574755be268
Emanalabsi <emanabsi30@gmail.com>
2023-06-13 14:32:48 +0300

    fix: resolve conflict

--- server/models/index.ts
+++ server/models/index.ts
@@ -4,4 +4,4 @@ import { Player } from './Player';
 import { Message } from './messages';
 import Review from './Review';
 
-export { Player, Stadium, Review, Match,Message };
+export { Player, Stadium, Review, Match, Message };
