69a026c6ddcc708fb8403d7f07d5c686f0bbc73a
Emanalabsi <emanabsi30@gmail.com>
2023-06-13 13:33:00 +0300

    feat: create matches model

--- server/models/Match.ts
+++ server/models/Match.ts
@@ -1,43 +1,38 @@
-// import { Model, DataTypes } from 'sequelize';
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../database';
 
-// class Match extends Model {
-//   declare owner: string;
-//   declare title: Text;
-//   declare desctiption: Text;
-//   declare time: Date;
-//   declare seats: boolean;
-//   declare players_id: number;
-// }
 
-// Match.init(
-//   {
-//     owner: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     title: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     desctiption: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     time: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     seats: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     players_id: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//   },
-//   { sequelize, modelName: 'Match', tableName: 'matches' },
-// );
+class MatchAttributes extends Model {
+  declare owner: string;
+  declare title: string;
+  declare desctiption: string;
+  declare time: Date;
+  declare seats: boolean;
+  declare players_id: number;
+}
 
-// export default Match;
-export {};
+export const Match = MatchAttributes.init(
+  {
+    owner: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    desctiption: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    time: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    seats: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  { sequelize, modelName: 'Match', tableName: 'matches' },
+);
--- server/models/index.ts
+++ server/models/index.ts
@@ -1,15 +1,5 @@
-// import Match from "./Match";
-
-// Match.belongsToMany(Player, {through:{'Match_Player'}})
-// Player.belongsToMany(Match, {through:{'Match_Player'}})
-
-// Player.hasMany(Review)
-// Stadium.hasMany(Review)
-
-// Player.hasMany(Chat)
-
-export {};
 import { Stadium } from './Stadium';
-import { Player } from './Player';
+import {Match} from "./Match";
+import { Player } from "./Player"
 
-export { Player, Stadium };
+export { Player, Stadium, Match };
