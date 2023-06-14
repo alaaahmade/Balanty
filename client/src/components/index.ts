8a30aa979778733eb20857cfce62b6396b1ed44d
Heba-Adwan <hebawael1575@gmail.com>
2023-06-14 09:34:20 +0300

    build: create contact us component and make styled component Relates #29

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,1 +1,22 @@
+export {
+  StyledContainer,
+  StyledBox,
+  StyledTypography,
+  StyledTextField,
+  StyledButton,
+  StyledEmailIcon,
+  StyledPhoneIcon,
+} from './styledRootComponent/ContactUsStyledComponent';
+
+export {
+  StyledDefinitionSection,
+  StyledTitle,
+  StyledDescription,
+} from './styledRootComponent/DefinitionStyledComponent';
+
+export {
+  StyledBoxFeatures,
+  StyledImageFeatures,
+  StyledDescriptionFeatures,
+} from './styledRootComponent/FeaturesStyledComponent';
 export {};
