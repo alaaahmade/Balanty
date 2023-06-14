058b9d5eaa6ccf70ff80ac6ac7d73bc5f6b7dd54
Heba-Adwan <hebawael1575@gmail.com>
2023-06-14 10:52:53 +0300

    build: definition who we are and styled mui Relates #28

---
+++ client/src/components/Definition.tsx
@@ -0,0 +1,67 @@
+// import React from 'react';
+// import {
+//   StyledDefinitionSection,
+//   StyledTitle,
+//   StyledDescription,
+// } from './index';
+
+// interface DefinitionProps {
+//   title: string;
+//   description: string;
+// }
+
+// const DefinitionSection: React.FC<DefinitionProps> = ({
+//   title,
+//   description,
+// }) => {
+//   return (
+//     <div>
+//       <StyledDefinitionSection>
+//         <StyledTitle>{title}</StyledTitle>
+//         <StyledDescription>{description}</StyledDescription>
+//       </StyledDefinitionSection>
+//     </div>
+//   );
+// };
+/* 
+<DefinitionSection
+        title="من نحن"
+        description="بلنتي هو موقع حجوزات ملاعب كرة القدم الذي يوفر للمستخدمين وسيلة سهلة ومريحة لحجز الملاعب عبر الإنترنت. يقدم بلنتي مجموعة متنوعة من الملاعب المتاحة للحجز في مختلف المناطق، مما يسمح للمستخدمين بالعثور على الملعب المثالي لاحتياجاتهم. باستخدام بلنتي، يمكن للمستخدمين تصفح قائمة الملاعب المتاحة وعرض التفاصيل الكاملة لكل ملعب، بما في ذلك الموقع، وحجم الملعب، ووسائل الراحة المتاحة، وأوقات الحجز المتاحة. كما يوفر الموقع نظامًا مبسطًا لحجز الملاعب حيث يمكن للمستخدمين اختيار التاريخ والوقت المفضل للحجز وإكمال عملية الحجز بسهولة وسرعة."
+      />
+*/
+import React from 'react';
+import {
+  StyledDefinitionSection,
+  StyledTitle,
+  StyledDescription,
+} from './index';
+
+interface DefinitionProps {
+  title: string;
+  description: string;
+}
+
+const DefinitionSection: React.FC<DefinitionProps> = ({
+  title,
+  description,
+}) => {
+  return (
+    <StyledDefinitionSection>
+      <StyledTitle>{title}</StyledTitle>
+      <StyledDescription>{description}</StyledDescription>
+    </StyledDefinitionSection>
+  );
+};
+
+const Definition: React.FC = () => {
+  return (
+    <div>
+      <DefinitionSection
+        title="من نحن"
+        description="بلنتي هو موقع حجوزات ملاعب كرة القدم الذي يوفر للمستخدمين وسيلة سهلة ومريحة لحجز الملاعب عبر الإنترنت. يقدم بلنتي مجموعة متنوعة من الملاعب المتاحة للحجز في مختلف المناطق، مما يسمح للمستخدمين بالعثور على الملعب المثالي لاحتياجاتهم. باستخدام بلنتي، يمكن للمستخدمين تصفح قائمة الملاعب المتاحة وعرض التفاصيل الكاملة لكل ملعب، بما في ذلك الموقع، وحجم الملعب، ووسائل الراحة المتاحة، وأوقات الحجز المتاحة. كما يوفر الموقع نظامًا مبسطًا لحجز الملاعب حيث يمكن للمستخدمين اختيار التاريخ والوقت المفضل للحجز وإكمال عملية الحجز بسهولة وسرعة."
+      />
+    </div>
+  );
+};
+
+export default Definition;
---
+++ client/src/components/styledRootComponent/DefinitionStyledComponent.tsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const StyledDefinitionSection = styled.div`
+  background: #f2fcf5;
+  height: 418px;
+`;
+
+export const StyledTitle = styled.h2`
+  font-weight: 700;
+  font-size: 48px;
+  margin-bottom: 30px;
+  padding-top: 60px;
+`;
+
+export const StyledDescription = styled.p`
+  margin-left: 246px;
+  margin-right: 246px;
+  text-align: center;
+`;
