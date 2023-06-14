b4deb85566546e0dea9cc61a4a2d890aaddcfe15
Emanalabsi <emanabsi30@gmail.com>
2023-06-14 22:55:51 +0300

    fix: create initial routefor signup

--- server/controllers/user/signup.ts
+++ server/controllers/user/signup.ts
@@ -1,1 +1,9 @@
+// import { NextFunction, Request, Response } from 'express';
+
+// const signup = (req: Request, res: Response, next: NextFunction) => {
+//   const req.body =
+// };
+
+// export default signup;
+
 export {};
---
+++ server/routes/user.ts
@@ -0,0 +1,9 @@
+// import express from 'express';
+
+// import { errorWrapper } from '../utils/errorHandler';
+
+// export const authRouter = express.Router();
+
+// authRouter.post('/signup', errorWrapper(signup));
+
+export {};
