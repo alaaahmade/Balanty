831cd4675ac1d6c7bca6e278980ec699f2ea41b1
alaaahmade <alaaahmade66@gmail.com>
2023-06-15 10:35:23 +0300

    fix: rename database Folder

---
+++ server/database/config/build.ts
@@ -0,0 +1,8 @@
+import { sequelize } from '..';
+import insertData from './InsertData';
+
+const build = async (): Promise<void> => {
+  await sequelize.sync({ force: true, logging: true });
+  await insertData();
+};
+export default build;
