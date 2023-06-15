fbe64a3b264beb84bd6426414cbdff186e590d15
Emanalabsi <emanabsi30@gmail.com>
2023-06-15 22:47:53 +0300

    fix: remove promisfy import

--- server/utils/jwt/generateToken.ts
+++ server/utils/jwt/generateToken.ts
@@ -1,7 +1,4 @@
 import { sign, Secret } from 'jsonwebtoken';
-import { promisify } from 'util';
-
-// const aa =  promisify()
 
 const generateToken = (payload: object): Promise<string> => {
   return new Promise<string>((resolve, reject) => {
