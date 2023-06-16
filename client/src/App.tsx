f8269a696187f12bfe367a576b419f6be3a4873f
unknown <ahmedirheem2003@gmail.com>
2023-06-16 16:28:17 +0300

    build: build header section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import { ReactElement } from 'react';
 import { ThemeProvider } from '@emotion/react';
 import RootLayout from './layouts/RootLayout';
 import LightTheme from './themes';
+import {Header} from './components';
 
 const App = (): ReactElement => {
   return (
     <ThemeProvider theme={LightTheme}>
-      <RootLayout />
+      {/* <RootLayout /> */}
+      <Header key={null} type={''} props={undefined} />
     </ThemeProvider>
   );
 };
