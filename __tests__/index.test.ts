d64298e8fb28a3b5130a1dbf936af27cf6239686
alaaahmade <alaaahmade66@gmail.com>
2023-06-16 15:45:28 +0300

    fix: github error

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,5 +1,21 @@
-describe('test GitHub Actions CICD Piplines', () => {
+import * as request from 'supertest';
+import app from '../server/app';
+
+describe('test GitHub Actions CICD Pipelines', () => {
   test('test for husky', () => {
     expect(3).toBe(3);
+  });
+});
+
+describe('Post /match/create', () => {
+  it('respond with json', done => {
+    request(app)
+      .get('/users')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
   });
 });
