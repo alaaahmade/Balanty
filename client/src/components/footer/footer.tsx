e0725ef9a4739518b31d2365bd270a1e404b8e48
Heba-Adwan <hebawael1575@gmail.com>
2023-06-17 13:13:23 +0300

    build: use mui material and icons style in footer

--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -10,37 +10,39 @@ import {
 
 const Footer: React.FC = (): ReactElement => {
   return (
-    <StyledFooter>
-      <StyledLogo>
-        <Typography variant="h6">بلنتي لوغو</Typography>
-      </StyledLogo>
-      <StyledLinks>
-        <Link href="https://example.com/contact" color="inherit">
-          تواصل معنا
-        </Link>
-        <Link href="https://example.com/services" color="inherit">
-          خدماتنا
-        </Link>
-        <Link href="https://example.com/about" color="inherit">
-          عن موقعنا
-        </Link>
-        <Link href="https://example.com/home" color="inherit">
-          الرئيسية
-        </Link>
-      </StyledLinks>
-      <Typography variant="body2" align="center">
-        بلنتي هو موقع حجوزات ملاعب كرة القدم وتشبيك اللاعبين ببعضهم الذي يوفر
-        للمستخدمين وسيلة سهلة ومريحة لحجز الملاعب عبر الإنترنت. يقدم بلنتي
-        مجموعة متنوعة من الملاعب المتاحة للحجز في مختلف المناطق، مما يسمح
-        للمستخدمين بالعثور على الملعب المثالي لاحتياجاتهم.
-      </Typography>
-      <StyledIcons>
-        <Facebook />
-        <Twitter />
-        <LinkedIn />
-        <Instagram />
-      </StyledIcons>
-    </StyledFooter>
+    <Box>
+      <StyledFooter>
+        <StyledLogo>
+          <Typography variant="h6">بلنتي لوغو</Typography>
+        </StyledLogo>
+        <StyledLinks>
+          <Link href="https://example.com/contact" color="inherit">
+            تواصل معنا
+          </Link>
+          <Link href="https://example.com/services" color="inherit">
+            خدماتنا
+          </Link>
+          <Link href="https://example.com/about" color="inherit">
+            عن موقعنا
+          </Link>
+          <Link href="https://example.com/home" color="inherit">
+            الرئيسية
+          </Link>
+        </StyledLinks>
+        <Typography variant="body2" align="center">
+          بلنتي هو موقع حجوزات ملاعب كرة القدم وتشبيك اللاعبين ببعضهم الذي يوفر
+          للمستخدمين وسيلة سهلة ومريحة لحجز الملاعب عبر الإنترنت. يقدم بلنتي
+          مجموعة متنوعة من الملاعب المتاحة للحجز في مختلف المناطق، مما يسمح
+          للمستخدمين بالعثور على الملعب المثالي لاحتياجاتهم.
+        </Typography>
+        <StyledIcons>
+          <Facebook />
+          <Twitter />
+          <LinkedIn />
+          <Instagram />
+        </StyledIcons>
+      </StyledFooter>
+    </Box>
   );
 };
 
