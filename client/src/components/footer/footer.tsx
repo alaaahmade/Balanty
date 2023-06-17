286633d036a68549ae4af729a1e332e5ed43d06c
Heba-Adwan <hebawael1575@gmail.com>
2023-06-17 12:57:56 +0300

    build: footer section links and description and logo

--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -5,21 +5,24 @@ import { Facebook, Twitter, LinkedIn, Instagram } from '@mui/icons-material';
 const Footer: React.FC = (): ReactElement => {
   return (
     <Box>
-      <Typography variant="h6">Logo</Typography>
-      <Link href="https://example.com/about" color="inherit">
-        About Us
-      </Link>
+      <Typography variant="h6">بلنتي لوغو</Typography>
       <Link href="https://example.com/contact" color="inherit">
-        Contact Us
-      </Link>
-      <Link href="https://example.com/home" color="inherit">
-        Home
+        تواصل معنا
       </Link>
       <Link href="https://example.com/services" color="inherit">
-        Services
+        خدماتنا
+      </Link>
+      <Link href="https://example.com/about" color="inherit">
+        عن موقعنا
+      </Link>
+      <Link href="https://example.com/home" color="inherit">
+        الرئيسية
       </Link>
       <Typography variant="body2" align="center">
-        Welcome all
+        بلنتي هو موقع حجوزات ملاعب كرة القدم وتشبيك اللاعبين ببعضهم الذي يوفر
+        للمستخدمين وسيلة سهلة ومريحة لحجز الملاعب عبر الإنترنت. يقدم بلنتي
+        مجموعة متنوعة من الملاعب المتاحة للحجز في مختلف المناطق، مما يسمح
+        للمستخدمين بالعثور على الملعب المثالي لاحتياجاتهم.
       </Typography>
       <Facebook />
       <Twitter />
