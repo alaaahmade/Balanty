d78dbda1b9c55415797ab16cb4f2905ed4c9649b
Emanalabsi <emanabsi30@gmail.com>
2023-06-18 16:29:16 +0300

    fix: remove secret key from jwt and move it to enviorment

--- server/config/environment.ts
+++ server/config/environment.ts
@@ -4,6 +4,7 @@ import { Dialect } from 'sequelize';
 const { DEV_DB_URL, DATABASE_URL, TEST_URL, NODE_ENV } = process.env;
 
 export const { PORT } = process.env || 8080;
+export const { SECRET_KEY } = process.env;
 
 interface connectionOption {
   dialect: Dialect;
