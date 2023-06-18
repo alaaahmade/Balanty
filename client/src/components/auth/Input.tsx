a91fa2e51498d0cdda247bbe988fd785d0ec6389
unknown <ahmedirheem2003@gmail.com>
2023-06-18 10:49:49 +0300

    build: build input text field

---
+++ client/src/components/auth/Input.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import { FormControl, OutlinedInput } from '@mui/material';
+import { InputLabel } from './Login.styled';
+
+interface Props {
+  type: string;
+  label: string;
+  placeholder: string;
+}
+const InputWrap: FC<Props> = ({ type, label, placeholder }) => {
+  return (
+    <div style={{ marginBottom: '1rem' }}>
+      <InputLabel>{label}</InputLabel>
+      <FormControl sx={{ width: '30ch' }}>
+        <OutlinedInput
+          sx={{ borderColor: '#2cb674' }}
+          type={type}
+          placeholder={placeholder}
+        />
+      </FormControl>
+    </div>
+  );
+};
+
+export default InputWrap;
