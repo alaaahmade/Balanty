feaa148a31de49d4af7444e82e9f25f43a10f253
unknown <ahmedirheem2003@gmail.com>
2023-06-18 13:08:35 +0300

    build: build routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,20 @@
-import { ReactElement } from 'react';
-import { ThemeProvider } from '@emotion/react';
-import RootLayout from './layouts/RootLayout';
-import LightTheme from './themes';
-import {Header} from './components';
+import { createBrowserRouter } from 'react-router-dom';
 
-const App = (): ReactElement => {
-  return (
-    <ThemeProvider theme={LightTheme}>
-      {/* <RootLayout /> */}
-      <Header key={null} type={''} props={undefined} />
-    </ThemeProvider>
-  );
-};
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Landing Page</h1>,
+  },
+  {
+    path: '/home',
+    element: <h1>Home Page</h1>,
+    children: [],
+  },
+  { path: '/player/login', element: <h1>player login</h1> },
+  { path: '/stadium/login', element: <h1>stadium login</h1> },
+  { path: '/player/signup', element: <h1>player signup</h1> },
+  { path: '/stadium/signup', element: <h1>stadium signup</h1> },
+  { path: '*', element: <h1>error</h1> },
+]);
 
-export default App;
+export default router;
