da767f8e810228e332b94c6c473820bf223471f1
unknown <ahmedirheem2003@gmail.com>
2023-06-18 11:13:47 +0300

    build: create wrapper styled

---
+++ client/src/components/auth/LoginWrapper.styled.tsx
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+import { Box } from '@mui/system';
+import { Typography, Button } from '@mui/material';
+
+interface Props {
+  isPlayer: boolean;
+}
+export const Wrapper = styled(Box)<Props>(({ isPlayer }) => ({
+  width: '100%',
+  height: '100vh',
+  display: 'flex',
+  flexDirection: isPlayer ? 'row' : 'row-reverse',
+  alignItems: 'center',
+  direction: 'rtl',
+}));
+
+export const ImageWrap = styled(Box)<Props>(({ isPlayer }) => ({
+  width: '50vw',
+  height: '100vh',
+  backgroundImage: isPlayer
+    ? 'url(https://img.freepik.com/premium-photo/football-scene-with-soccer-players-stadium-with-technology-analysis_207634-6513.jpg)'
+    : 'url(https://img.freepik.com/premium-photo/close-up-soccer-ball-center-stadium-illuminated-by-headlights_207634-2741.jpg)',
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+}));
