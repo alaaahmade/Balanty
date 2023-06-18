169fc153b0dc6c3cff0f2933df1381aee040183f
unknown <ahmedirheem2003@gmail.com>
2023-06-18 11:19:07 +0300

    build: login page component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,12 @@
 import { ReactElement } from 'react';
 import { ThemeProvider } from '@emotion/react';
-import RootLayout from './layouts/RootLayout';
 import LightTheme from './themes';
-import {Header} from './components';
+import LoginWrapper from './components/auth/LoginWrapper';
 
 const App = (): ReactElement => {
   return (
     <ThemeProvider theme={LightTheme}>
-      {/* <RootLayout /> */}
-      <Header key={null} type={''} props={undefined} />
+      <LoginWrapper isPlayer isSignup />
     </ThemeProvider>
   );
 };
