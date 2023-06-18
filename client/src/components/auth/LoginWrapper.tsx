44706460912ef3ac4704472b9c31e1d1133c13dd
unknown <ahmedirheem2003@gmail.com>
2023-06-18 11:11:53 +0300

    build: build info form

--- client/src/components/auth/LoginWrapper.tsx
+++ client/src/components/auth/LoginWrapper.tsx
@@ -18,7 +18,53 @@ const LoginWrapper: FC<Props> = ({ isPlayer, isSignup }): ReactElement => {
       <ImageWrap isPlayer={isPlayer} />
       <Form>
         <TitleWrap isPlayer={isPlayer} isSignup={isSignup} />
-
+        <Box component="form" noValidate autoComplete="off">
+          <InputWrap
+            type="text"
+            label={isPlayer ? 'اسم اللاعب' : 'اسم الملعب'}
+            placeholder={isPlayer ? 'ادخل اسم اللاعب' : 'ادخل اسم الملعب'}
+          />
+          {isSignup && (
+            <InputWrap
+              type="email"
+              label="البريد الإلكتروني"
+              placeholder="ادخل البريد الإلكتروني"
+            />
+          )}
+          <InputWrap
+            type="password"
+            label="كلمة المرور"
+            placeholder="ادخل كلمة المرور"
+          />
+          {isSignup && (
+            <InputWrap
+              type="password"
+              label="تأكيد كلمة المرور"
+              placeholder="قم بتأكيد كلمة المرور"
+            />
+          )}
+          <SignButton variant="contained" disableElevation>
+            {isSignup ? 'سجل الان' : 'تسجيل دخول'}
+          </SignButton>
+          <LinkWrap isPlayer={isPlayer} isSignup={isSignup} />
+          <SignButton
+            sx={{ background: '#E6E6E6 !important', color: '#868686' }}
+            variant="contained"
+            disableElevation
+          >
+            {isSignup ? 'سجِل من خلال' : 'تسجيل دخول من خلال'}
+            <img
+              style={{
+                width: '20px',
+                height: '20px',
+                verticalAlign: 'middle',
+                marginRight: '0.7rem',
+              }}
+              src={GoogleIcon}
+              alt="Google Icon"
+            />
+          </SignButton>
+        </Box>
       </Form>
     </Wrapper>
   );
