6af3bf59158d46db8f0a15aa3a4cf79afbf1d0b0
alaaahmade <alaaahmade66@gmail.com>
2023-06-19 10:13:11 +0300

    fix: resolve conflict

--- server/dataBase/config/build.ts
+++
@@ -1,8 +0,0 @@
-import insertData from './insertData';
-import { sequelize } from '..';
-
-const build = async (): Promise<void> => {
-  await sequelize.sync({ force: true, logging: true });
-  await insertData();
-};
-export default build;
