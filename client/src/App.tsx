7a7d267eab34427945f9db5bcc1b42df41ca9740
unknown <ahmedirheem2003@gmail.com>
2023-06-19 12:48:24 +0300

    build: build Auth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import { ReactElement } from 'react';
 import { ThemeProvider } from '@emotion/react';
 import LightTheme from './themes';
 import LoginWrapper from './components/auth/LoginWrapper';
+import SignupWrapper from './components/auth/SignupWrapper';
 
 const App = (): ReactElement => {
   return (
     <ThemeProvider theme={LightTheme}>
-      <LoginWrapper isPlayer isSignup />
+      <LoginWrapper isPlayer={false} />
+      {/* <SignupWrapper isPlayer/> */}
     </ThemeProvider>
   );
 };
