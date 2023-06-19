15b718e071b95bb598c73b461a386bffa07162d7
Heba-Adwan <hebawael1575@gmail.com>
2023-06-19 09:44:58 +0300

    fix: fix style in footer

--- client/src/components/footer/footer.styled.tsx
+++ client/src/components/footer/footer.styled.tsx
@@ -1,6 +1,7 @@
-import { styled } from '@mui/system';
+import { styled, typography } from '@mui/system';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import { Link } from '@mui/material';
 
 export const StyledFooter = styled('footer')`
   background-color: #01031c;
@@ -11,21 +12,40 @@ export const StyledFooter = styled('footer')`
   align-items: center;
 `;
 
-export const StyledLogo = styled(Box)`
-  margin-bottom: 10px;
+export const StyledLogo = styled(Typography)`
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 22px;
+  line-height: 26px;
+  text-align: center;
+  color: #ffffff;
+  margin-bottom: 30px;
 `;
 
-export const StyledLinks = styled(Box)`
+export const StyledLinks = styled(Link)`
   display: flex;
-  margin-bottom: 10px;
+  color: #ffffff;
+  margin-bottom: 30px;
+  text-decoration: none;
 
   & > * {
     margin: 0 10px;
+    text-decoration: none;
+  }
+
+  & > *:last-child {
+    margin-right: 0;
   }
 `;
-
+export const StyledTypographyParagraph = styled(typography)`
+  fontsize: 15px;
+  color: #dddddd;
+  text-align: center;
+`;
 export const StyledIcons = styled(Box)`
   display: flex;
+  margin-top: 30px;
 
   & > * {
     margin: 0 5px;
--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -10,39 +10,37 @@ import {
 
 const Footer: React.FC = (): ReactElement => {
   return (
-    <Box>
-      <StyledFooter>
-        <StyledLogo>
-          <Typography variant="h6">بلنتي لوغو</Typography>
-        </StyledLogo>
-        <StyledLinks>
-          <Link href="https://example.com/contact" color="inherit">
-            تواصل معنا
-          </Link>
-          <Link href="https://example.com/services" color="inherit">
-            خدماتنا
-          </Link>
-          <Link href="https://example.com/about" color="inherit">
-            عن موقعنا
-          </Link>
-          <Link href="https://example.com/home" color="inherit">
-            الرئيسية
-          </Link>
-        </StyledLinks>
-        <Typography variant="body2" align="center">
-          بلنتي هو موقع حجوزات ملاعب كرة القدم وتشبيك اللاعبين ببعضهم الذي يوفر
-          للمستخدمين وسيلة سهلة ومريحة لحجز الملاعب عبر الإنترنت. يقدم بلنتي
-          مجموعة متنوعة من الملاعب المتاحة للحجز في مختلف المناطق، مما يسمح
-          للمستخدمين بالعثور على الملعب المثالي لاحتياجاتهم.
-        </Typography>
+    <StyledFooter>
+      <StyledLogo variant="h6">بلنتي لوغو</StyledLogo>
+      <StyledLinks>
+        <Link href="https://example.com/contact" color="inherit">
+          تواصل معنا
+        </Link>
+        <Link href="https://example.com/services" color="inherit">
+          خدماتنا
+        </Link>
+        <Link href="https://example.com/about" color="inherit">
+          عن موقعنا
+        </Link>
+        <Link href="https://example.com/home" color="inherit">
+          الرئيسية
+        </Link>
+      </StyledLinks>
+      <Typography variant="body2" align="center">
+        بلنتي هو موقع حجوزات ملاعب كرة القدم وتشبيك اللاعبين ببعضهم الذي يوفر
+        للمستخدمين وسيلة سهلة ومريحة لحجز الملاعب عبر الإنترنت. <br />
+        يقدم بلنتي مجموعة متنوعة من الملاعب المتاحة للحجز في مختلف المناطق، مما
+        يسمح للمستخدمين بالعثور على الملعب المثالي لاحتياجاتهم.
+      </Typography>
+      <Box>
         <StyledIcons>
           <Facebook />
           <Twitter />
           <LinkedIn />
           <Instagram />
         </StyledIcons>
-      </StyledFooter>
-    </Box>
+      </Box>
+    </StyledFooter>
   );
 };
 
