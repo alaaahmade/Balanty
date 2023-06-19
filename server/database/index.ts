4a2e0f94d228d1d9e2c1d87aa229f9ba07080986
alaaahmade <alaaahmade66@gmail.com>
2023-06-19 14:27:31 +0300

    fix: resolve conflict

--- server/database/index.ts
+++ server/database/index.ts
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
 import { build, sequelize } from './config';
 export { sequelize, build };
-=======
-import { sequelize } from '../database/config';
-
-export { sequelize };
->>>>>>> main
