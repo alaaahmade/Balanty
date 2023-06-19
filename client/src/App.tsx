ed505e58cbe100a3a13e94ce481fec3312830321
unknown <ahmedirheem2003@gmail.com>
2023-06-19 09:56:10 +0300

    fix: fix routes tree

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,22 @@
 import { createBrowserRouter } from 'react-router-dom';
+import RootLayout from './layouts/RootLayout';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <h1>Landing Page</h1>,
+    children: [
+      { path: '/player/login', element: <h1>player login</h1> },
+      { path: '/stadium/login', element: <h1>stadium login</h1> },
+      { path: '/player/signup', element: <h1>player signup</h1> },
+      { path: '/stadium/signup', element: <h1>stadium signup</h1> },
+    ],
   },
   {
     path: '/home',
-    element: <h1>Home Page</h1>,
+    element: <RootLayout />,
     children: [],
   },
-  { path: '/player/login', element: <h1>player login</h1> },
-  { path: '/stadium/login', element: <h1>stadium login</h1> },
-  { path: '/player/signup', element: <h1>player signup</h1> },
-  { path: '/stadium/signup', element: <h1>stadium signup</h1> },
   { path: '*', element: <h1>error</h1> },
 ]);
 
