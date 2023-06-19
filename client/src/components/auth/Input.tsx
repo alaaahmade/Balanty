c53f2319c895efc8f2a83e6197c16dde54261769
unknown <ahmedirheem2003@gmail.com>
2023-06-19 16:00:25 +0300

    fix: update Input components

--- client/src/components/auth/Input.tsx
+++ client/src/components/auth/Input.tsx
@@ -11,9 +11,18 @@ const InputWrap: FC<Props> = ({ type, label, placeholder }) => {
   return (
     <div style={{ marginBottom: '1rem' }}>
       <InputLabel>{label}</InputLabel>
-      <FormControl sx={{ width: '30ch' }}>
+      <FormControl sx={{ width: '45ch' }}>
         <OutlinedInput
-          sx={{ borderColor: '#2cb674' }}
+          className="outline-input"
+          sx={{
+            '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+              borderColor: '#2cb674',
+            },
+            '& input': {
+              height: '0.8em', // Adjust the height value as needed
+              caretColor: '#2cb674',
+            },
+          }}
           type={type}
           placeholder={placeholder}
         />
