1b952579f7a9d5d09b5d9c92b98cb18156d879e9
alaaahmade <alaaahmade66@gmail.com>
2023-06-19 12:49:27 +0300

    fix: resolve chenge request

--- server/services/matches.ts
+++ server/services/matches.ts
@@ -5,13 +5,20 @@ import matchSchema from '../validations';
 const createMatchService = async (req: CustomRequest): Promise<unknown> => {
   const { body, userData } = req;
   const owner_id = userData?.owner_id;
-  const { startDate } = body;
-  const Exist = await Match.findOne({ where: { owner_id, startDate } });
+  const { StadiumId, startDate } = body;
+
+  console.log({ StadiumId, startDate });
+
+  const Exist = await Match.findOne({
+    where: { startDate, user_id: StadiumId },
+  });
   if (!Exist) {
     const data = await matchSchema.validateAsync(body);
     const DBData = await Match.create({ ...data, owner_id });
     return DBData;
   }
+  console.log(Exist);
+
   return '! هذا الوقت محجوز';
 };
 
