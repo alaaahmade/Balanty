b4d7083b11ce9160c75a9ed3b0f3e221369158c6
unknown <ahmedirheem2003@gmail.com>
2023-06-19 16:43:21 +0300

    build: add Auth pages to routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
 import RootLayout from './layouts/RootLayout';
 import { LandingPage } from './pages';
+import LoginWrapper from './components/auth/LoginWrapper';
+import SignupWrapper from './components/auth/SignupWrapper';
 
 const router = createBrowserRouter([
   {
     path: '/',
     children: [
       { index: true, element: <LandingPage /> },
-      { path: '/player/login', element: <h1>player login</h1> },
-      { path: '/stadium/login', element: <h1>stadium login</h1> },
-      { path: '/player/signup', element: <h1>player signup</h1> },
-      { path: '/stadium/signup', element: <h1>stadium signup</h1> },
+      { path: '/player/login', element: <LoginWrapper /> },
+      { path: '/stadium/login', element: <LoginWrapper /> },
+      { path: '/player/signup', element: <SignupWrapper /> },
+      { path: '/stadium/signup', element: <SignupWrapper /> },
     ],
   },
   {
