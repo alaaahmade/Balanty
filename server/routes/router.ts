8efdbe4ea71658956cddbfb1c3981aa5e4b80e7d
Heba-Adwan <hebawael1575@gmail.com>
2023-06-20 11:14:15 +0300

    feat: add roouter

--- server/routes/router.ts
+++ server/routes/router.ts
@@ -1,17 +1,17 @@
 import { Router, Request, Response } from 'express';
-import matchRouter from './matches';
+import { matchRouter } from './matches';
 import { authRouter } from './auth';
 
 const router: Router = Router();
 
+console.log('hi jeebadddd', matchRouter);
+router.use('/matches', matchRouter);
+router.use('/user', authRouter);
 router.get('/', (req: Request, res: Response): void => {
   res.json({
     status: 200,
     msg: 'ok',
   });
 });
-
-router.use('/matches', matchRouter);
-router.use('/user', authRouter);
 
 export { router };
