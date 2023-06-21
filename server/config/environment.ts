c82b0ad682d985cb0b10bac01224f3feffc97a4c
unknown <ahmedirheem2003@gmail.com>
2023-06-21 05:04:31 +0300

    fix: fix secrete key problem

--- server/config/environment.ts
+++ server/config/environment.ts
@@ -4,12 +4,16 @@ import { Dialect } from 'sequelize';
 const { DEV_DB_URL, DATABASE_URL, TEST_URL, NODE_ENV, SECRET_KEY } =
   process.env;
 
+if (!NODE_ENV) {
+  throw new Error('Invalid Environment Variables');
+}
+export const nodeEnv = NODE_ENV;
 export const { PORT } = process.env || 8080;
-export const { SECRET_KEY } = process.env;
 
 interface connectionOption {
   dialect: Dialect;
-  dialectOptions?: {
+  dialectOptions: {
+    charset: string;
     ssl: boolean | object;
   };
 }
@@ -19,18 +23,22 @@ if (!NODE_ENV || !DATABASE_URL || !SECRET_KEY) {
 }
 export const sequelizeOption: connectionOption = {
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: false,
+    charset: 'utf8mb4',
+  },
 };
 
 export let dbUrl = '';
 
 if (NODE_ENV === 'production' && DATABASE_URL) {
   dbUrl = DATABASE_URL;
-  sequelizeOption.dialectOptions = { ssl: { rejectUnauthorized: false } };
+  sequelizeOption.dialectOptions.ssl = { rejectUnauthorized: false };
 } else if (NODE_ENV === 'development' && DEV_DB_URL) {
-  sequelizeOption.dialectOptions = { ssl: false };
+  sequelizeOption.dialectOptions.ssl = false;
   dbUrl = DEV_DB_URL;
 } else if (NODE_ENV === 'test' && TEST_URL) {
-  sequelizeOption.dialectOptions = { ssl: false };
+  sequelizeOption.dialectOptions.ssl = false;
   dbUrl = TEST_URL;
 } else {
   throw new Error('Invalid NODE_ENV');
