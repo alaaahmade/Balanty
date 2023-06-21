cde16a1375a5e08393cd3201f494e2c433234487
unknown <ahmedirheem2003@gmail.com>
2023-06-21 05:02:06 +0300

    fix: fix database building

--- server/database/config/build.ts
+++ server/database/config/build.ts
@@ -1,8 +1,12 @@
 import { sequelize } from '..';
+import { nodeEnv } from '../../config';
 import insertData from './insertData';
 
 const build = async (): Promise<void> => {
   await sequelize.sync({ force: true, logging: false });
   await insertData();
 };
+if (nodeEnv !== 'test') {
+  build();
+}
 export default build;
