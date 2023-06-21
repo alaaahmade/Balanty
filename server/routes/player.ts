e79bf4441bf8542a5d5d32c3d0d0d80cb72257bd
Emanalabsi <emanabsi30@gmail.com>
2023-06-22 00:42:27 +0300

    build: add update profile endpoint

--- server/routes/player.ts
+++ server/routes/player.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import { getPlayer } from '../controllers/player';
+import { getPlayer, updatePlayer } from '../controllers/player';
 import { errorWrapper } from '../utils';
 
 export const playerRouter: Router = Router();
 
 playerRouter.get('/profile/:id', errorWrapper(getPlayer));
-playerRouter.put("/profile/:id", errorWrapper(updatePlayer))
+playerRouter.put('/profile/:id', errorWrapper(updatePlayer));
