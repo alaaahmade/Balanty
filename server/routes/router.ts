0839558ef9a21fd9c3ce90d4ed727a02d9db8fea
Emanalabsi <emanabsi30@gmail.com>
2023-06-22 00:45:03 +0300

    build: use the player router

--- server/routes/router.ts
+++ server/routes/router.ts
@@ -2,11 +2,13 @@ import { Router, Request, Response } from 'express';
 import { stadiumRouter } from './staduimRouter';
 import matchRouter from './matchesRouter';
 import { authRouter } from './auth';
+import { playerRouter } from './player';
 
 const router: Router = Router();
 router.use('/user', authRouter);
 router.use('/matches', matchRouter);
 router.use('/stadiums', stadiumRouter);
+router.use('/players', playerRouter);
 
 router.get('/', (req: Request, res: Response): void => {
   res.json({
