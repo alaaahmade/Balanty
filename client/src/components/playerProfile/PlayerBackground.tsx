425081dfc41bb61b91faf777404711720af82a2e
Emanalabsi <emanabsi30@gmail.com>
2023-06-21 09:39:16 +0300

    feat: divide player profile components

---
+++ client/src/components/playerProfile/PlayerBackground.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+
+const PlayerBackground: React.FC = (): ReactElement => {
+  return (
+    <div>
+      <h1>Player Background Component</h1>
+      <p>This is the content of the PlayerBackground component.</p>
+    </div>
+  );
+};
+
+export default PlayerBackground;
