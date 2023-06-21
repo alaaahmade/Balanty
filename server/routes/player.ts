5856a9ec06ac4d2afad8ae1f654c793de69e7ebc
Emanalabsi <emanabsi30@gmail.com>
2023-06-21 23:12:55 +0300

    feat: create update profile route

--- server/routes/player.ts
+++ server/routes/player.ts
@@ -5,3 +5,4 @@ import { errorWrapper } from '../utils';
 export const playerRouter: Router = Router();
 
 playerRouter.get('/profile/:id', errorWrapper(getPlayer));
+playerRouter.put("/profile/:id", errorWrapper(updatePlayer))
