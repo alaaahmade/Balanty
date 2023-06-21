6ad615edcab61d1a45f9b2b5fa621777a501cbab
Emanalabsi <emanabsi30@gmail.com>
2023-06-21 10:58:05 +0300

    fix: change some styles in navbar

--- client/src/components/playerProfile/PlayerBackground.tsx
+++ client/src/components/playerProfile/PlayerBackground.tsx
@@ -1,3 +1,4 @@
+import { Avatar, Box } from '@mui/material';
 import React, { ReactElement } from 'react';
 import LeftSideBar from '../RootComponents/leftSideBar/LeftSideBar';
 import NavBar from '../RootComponents/navBar/NavBar';
@@ -6,8 +7,25 @@ const PlayerBackground: React.FC = (): ReactElement => {
   return (
     <div>
       <NavBar />
-      <div style={{ display: 'flex', right:0 }}>
-        hello
+      <div style={{ display: 'flex', right: 0 }}>
+        <div>
+          <Box
+            sx={{
+              width: 300,
+              height: 300,
+              backgroundColor: 'primary.dark',
+              '&:hover': {
+                backgroundColor: 'primary.main',
+                opacity: [0.9, 0.8, 0.7],
+              },
+            }}
+          />
+          <Avatar
+            alt="Remy Sharp"
+            src="/static/images/avatar/1.jpg"
+            sx={{ width: 56, height: 56 }}
+          />
+        </div>
         <LeftSideBar />
       </div>
     </div>
