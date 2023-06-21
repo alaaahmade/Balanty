c01a6afc609d2737495c8605586c91a763a56463
unknown <ahmedirheem2003@gmail.com>
2023-06-21 04:58:45 +0300

    build: build login service

--- server/services/auth.ts
+++ server/services/auth.ts
@@ -4,15 +4,13 @@ import { CustomError } from '../utils';
 import { signupSchema } from '../validations/schema';
 import { generateToken } from '../utils/jwt/generateToken';
 import { UserData } from '../interfaces/auth';
+import { loginSchema } from '../validations';
+import { userLoginAttrs } from '../interfaces/auth';
 
 const signupService = async (userData: UserData) => {
   const { username, email, password, phone, role } = userData;
 
-  const validatedData = await signupSchema.validateAsync(userData);
-
-  if (!validatedData) {
-    throw new CustomError(422, 'UnproccableContent');
-  }
+  await signupSchema.validateAsync(userData);
 
   const userExists = await User.findOne({ where: { username } });
 
@@ -35,4 +33,37 @@ const signupService = async (userData: UserData) => {
   return { newUser, token };
 };
 
-export { signupService };
+const loginService = async (userData: userLoginAttrs) => {
+  const { password, username } = userData;
+
+  await loginSchema.validateAsync(userData);
+
+  const user = await User.findOne({
+    where: { username },
+  });
+  if (!user) {
+    throw new CustomError(404, 'User Not Found');
+  }
+
+  const result = await bcrypt.compare(password, user.dataValues.password);
+
+  if (!result) {
+    throw new CustomError(401, 'Invalid Email or Password');
+  }
+
+  const userName = user.username;
+  const { id, email, phone, role, createdAt, updatedAt } = user.dataValues;
+
+  const loggedUser = {
+    id,
+    email,
+    phone,
+    role,
+    createdAt,
+    updatedAt,
+  };
+  const token = generateToken({ id, userName, email, phone, role });
+  return { loggedUser, token };
+};
+
+export { signupService, loginService };
