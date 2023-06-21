c0363cfae1a0515f182941b0444477ac36bc0462
unknown <ahmedirheem2003@gmail.com>
2023-06-21 05:30:39 +0300

    fix: fix checkAuth

--- server/middleware/checkAuth.ts
+++ server/middleware/checkAuth.ts
@@ -1,18 +1,14 @@
-import { NextFunction, Request, RequestHandler, Response } from 'express';
+import { NextFunction, Response } from 'express';
 import { CustomError, verifyToken } from '../utils';
 
-const checkAuth: RequestHandler = (
-  req: any,
-  res: Response,
-  next: NextFunction,
-) => {
+const checkAuth = async (req: any, res: Response, next: NextFunction) => {
   const { token } = req.cookies;
 
   if (!token) {
     throw new CustomError(400, 'Unauthorized');
   }
   try {
-    const decoded = verifyToken(token);
+    const decoded = await verifyToken(token);
     req.user = decoded;
     next();
   } catch (error) {
