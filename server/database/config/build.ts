06b9fa4d4848f9cf9cebd1a3d548bcc50238b980
heba adwan <93095734+Heba-Adwan@users.noreply.github.com>
2023-06-22 14:18:31 +0300

    fix: fix conflict in build.ts

--- server/database/config/build.ts
+++ server/database/config/build.ts
@@ -1,8 +1,14 @@
+mport { sequelize } from '..';
+import { BUILD_DB } from '../../config';
 import insertData from './insertData';
-import { sequelize } from '..';
 
 const build = async (): Promise<void> => {
   await sequelize.sync({ force: true, logging: false });
   await insertData();
 };
+
+if (BUILD_DB) {
+  build();
+}
+
 export default build;
