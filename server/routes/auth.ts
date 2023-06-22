79c9c7475bfa4cd3c84bc3dc95e75e95a908668d
unknown <ahmedirheem2003@gmail.com>
2023-06-22 13:27:03 +0300

    build: build logout route

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -2,7 +2,8 @@ import express, { Router } from 'express';
 export const authRouter: Router = express.Router();
 
 import { errorWrapper } from '../utils/errorHandler';
-import { signup, login } from '../controllers/auth';
+import { signup, login, logout } from '../controllers/auth';
 
 authRouter.post('/signup', errorWrapper(signup));
 authRouter.post('/login', errorWrapper(login));
+authRouter.post('/logout', logout);
