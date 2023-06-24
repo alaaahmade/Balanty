3ab1a27619497c249e42ea8b2af05618c8a9d46c
alaaahmade <alaaahmade66@gmail.com>
2023-06-24 17:34:57 +0300

    build: create 2 test for GET/stadiums/profile/5

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -98,7 +98,50 @@ describe('GET /api/v1/stadiums', () => {
     });
   });
 });
+describe('GET /api/v1/stadiums/profiles', () => {
+  test('responds from /api/v1/stadiums/profile/id with JSON and 200 status code', done => {
+    request(app)
+      .get('/api/v1/stadiums/profile/5')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        expect(res.status).toBe(200);
+        expect(res.type).toBe('application/json');
+        expect(typeof res).toBe('object');
+        const response = JSON.parse(res.text);
+        const { data } = response;
+        expect(response.status).toBe(200);
+        expect(data.username).toBe('ملعب الساحة');
+        expect(typeof data.Stadium).toBe('object');
+        expect(data.Stadium.id).toBe(1);
+        expect(Array.isArray(data.Stadium.stadiumGallery)).toBe(true);
+        expect(typeof data.Stadium.stadiumGallery[0]).toBe('object');
+        done();
 
+        if (err) {
+          done(err);
+        }
+      });
+  });
+
+  test('test for not exist Stadium should return 404 with "هذا الملعب غير متاح"', done => {
+    request(app)
+      .get('/api/v1/stadiums/profile/500')
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        if (err) {
+          done(err);
+        }
+        expect(res.status).toBe(401);
+        expect(res.type).toBe('application/json');
+        expect(typeof res).toBe('object');
+        const response = JSON.parse(res.text);
+        const { data } = response;
+        expect(response.status).toBe(401);
+        expect(data).toBe('هذا الملعب غير متاح');
+        done();
+      });
+  });
+});
 afterAll(async () => {
   sequelize.close();
 });
