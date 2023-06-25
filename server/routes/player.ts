a846b90d3492c6237025d5e0943c0802bd9bfc6a
Emanalabsi <emanabsi30@gmail.com>
2023-06-26 01:25:52 +0300

    build: create following & followers routes

--- server/routes/player.ts
+++ server/routes/player.ts
@@ -5,4 +5,10 @@ import { errorWrapper } from '../utils';
 export const playerRouter: Router = Router();
 
 playerRouter.get('/profile/:id', errorWrapper(getPlayer));
+playerRouter.get('/profile/followers', errorWrapper(getFollowers));
+playerRouter.get('/profile/following', errorWrapper(getFollowing));
+playerRouter.delete(
+  '/profile/following/unfollow',
+  errorWrapper(removeFollowing),
+);
 playerRouter.put('/profile/:id', errorWrapper(updatePlayer));
