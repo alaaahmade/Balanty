09eeb67422a5dc7d58d282d707749602cd66e4c1
unknown <ahmedirheem2003@gmail.com>
2023-06-25 16:42:24 +0300

    build: build delete message router

--- server/routes/matchChat.ts
+++ server/routes/matchChat.ts
@@ -12,5 +12,6 @@ const chatRouter: Router = Router();
 chatRouter.get('/', getAllMessages);
 chatRouter.get('/:id', getMessageById);
 chatRouter.post('/add', addMessage);
+chatRouter.delete('/:id', deleteMessage);
 
 export default chatRouter;
