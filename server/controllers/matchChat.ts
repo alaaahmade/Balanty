671bbf34488dd380ce3bc0ec6d80bfc22e43c2c4
unknown <ahmedirheem2003@gmail.com>
2023-06-25 16:26:17 +0300

    build: build add message controller

--- server/controllers/matchChat.ts
+++ server/controllers/matchChat.ts
@@ -1,23 +1,34 @@
-import { NextFunction, Request, Response } from 'express';
-import { addMessageService } from '../services';
+import { Request, Response } from 'express';
+import {
+  addMessageService,
+  deleteMessageService,
+  getAllMessagesService,
+  getMessageByIdService,
+  editMessageService,
+} from '../services';
 
-const addMessage = async (req: Request, res: Response, next: NextFunction) => {
-  const { message, senderId, matchId, username } = req.body;
+const addMessage = async (req: Request, res: Response) => {
+  const { message, senderId, matchId } = req.body;
 
   const newMessage = await addMessageService({
     message,
     senderId,
     matchId,
-    username,
   });
 
   res.status(201).json({
     status: 201,
     data: {
-      message: 'تم ارسال الرسالة بنجاح',
+      message: 'Message added successfully',
       newMessage,
     },
   });
 };
 
-export default addMessage;
+export {
+  addMessage,
+  // getMessageById,
+  // getAllMessages,
+  // deleteMessage,
+  // editMessage,
+};
