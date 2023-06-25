35b17b48acd7a826f7a3cd6399bb39623ec4e5af
Heba-Adwan <hebawael1575@gmail.com>
2023-06-25 11:17:32 +0300

    fix: remove uncomment

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,8 @@
 import app from './app';
 import { PORT } from './config';
-// import { sequelize } from './database/config/connection';
 
 const startServer = async (): Promise<void> => {
   try {
-    // await sequelize.authenticate();
     console.log('Connection has been established successfully.');
     app.listen(PORT, (): void => {
       console.log(`server is running on http://localhost:${PORT}`);
