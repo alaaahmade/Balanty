9a8465443bbcac9b3124238dd8bdb99a7fa7cb5c
unknown <ahmedirheem2003@gmail.com>
2023-06-25 16:43:26 +0300

    build: import/export all match chat services

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,7 +1,11 @@
-import addMessageService from './matchChat';
+export {
+  addMessageService,
+  getMessageByIdService,
+  getAllMessagesService,
+  deleteMessageService,
+  editMessageService,
+} from './matchChat';
 
 export { getStadiumMatchesService } from './stadiums';
 export { getAllStadiumsService, getStadiumDetailsService } from './stadiums';
 export { default as createMatchService } from './matches';
-
-export { addMessageService };
