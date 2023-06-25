363700f60d2f318476b0986bf5495aff70fbfd27
unknown <ahmedirheem2003@gmail.com>
2023-06-25 16:25:15 +0300

    build: import/export all match chat controllers

--- server/controllers/index.ts
+++ server/controllers/index.ts
@@ -1,7 +1,12 @@
-import addMessage from './matchChat';
+export {
+  addMessage,
+  getMessageById,
+  getAllMessages,
+  deleteMessage,
+  editMessage,
+} from './matchChat';
+
 export { getStadiumMatches } from './stadiums';
 export { getAllStadiums, getStadiumDetails } from './stadiums';
 
 export { default as createMatch } from './matches';
-
-export { addMessage };
