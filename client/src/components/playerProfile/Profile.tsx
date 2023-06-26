508992cedba39c3fbbe3445d2918d0fb46d8b5e9
Emanalabsi <emanabsi30@gmail.com>
2023-06-26 14:01:22 +0300

    fix: remove unused packages

--- client/src/components/playerProfile/Profile.tsx
+++ client/src/components/playerProfile/Profile.tsx
@@ -1,11 +1,9 @@
 import React, { ReactElement } from 'react';
-import { Box } from '@mui/material';
-import { FollowsInfoWrapper, FollowsWrapper } from './Player.Styled';
+import { FollowsInfoWrapper } from './Player.Styled';
 import PlayerActions from './PLayerActions';
 import PlayerBackground from './PlayerBackground';
 import PlayerStats from './PlayerStats';
 import PlayerInformation from './PlayerInformation';
-import PlayerFollows from './PlayerFollows';
 
 const Profile: React.FC = (): ReactElement => {
   return (
