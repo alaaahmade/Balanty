4b473fb04d1e8da744f4ce25e9ad8ea70f5a8548
Emanalabsi <emanabsi30@gmail.com>
2023-06-26 15:31:03 +0300

    feat: get player axios request

--- client/src/components/playerProfile/Profile.tsx
+++ client/src/components/playerProfile/Profile.tsx
@@ -1,4 +1,7 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { LineAxisOutlined } from '@mui/icons-material';
+import axios from 'axios';
 import { FollowsInfoWrapper } from './Player.Styled';
 import PlayerActions from './PLayerActions';
 import PlayerBackground from './PlayerBackground';
@@ -6,6 +9,19 @@ import PlayerStats from './PlayerStats';
 import PlayerInformation from './PlayerInformation';
 
 const Profile: React.FC = (): ReactElement => {
+  const { id } = useParams<{ id: string }>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`/api/v1/profile/${id}`);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [id]);
   return (
     <>
       <PlayerBackground />
