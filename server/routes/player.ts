a1edcab3323f490b5b481260cf3db938f741adc2
Emanalabsi <emanabsi30@gmail.com>
2023-06-29 01:10:05 +0300

    fix: change route of update player

--- server/routes/player.ts
+++ server/routes/player.ts
@@ -5,10 +5,4 @@ import { errorWrapper } from '../utils';
 export const playerRouter: Router = Router();
 
 playerRouter.get('/profile/:id', errorWrapper(getPlayer));
-// playerRouter.get('/profile/followers', errorWrapper(getFollowers));
-// playerRouter.get('/profile/following', errorWrapper(getFollowing));
-// playerRouter.delete(
-//   '/profile/following/unfollow',
-//   errorWrapper(removeFollowing),
-// );
 playerRouter.put('/profile/:id', errorWrapper(updatePlayer));
