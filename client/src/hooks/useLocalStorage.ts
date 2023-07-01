ea31f8e2137fdcc74ac3f63bc8da76de49c9fa47
unknown <ahmedirheem2003@gmail.com>
2023-07-01 17:22:43 +0300

    build: useLocalStorage context

---
+++ client/src/hooks/useLocalStorage.ts
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+const useLocalStorage = () => {
+  const [value, setValue] = useState<string | null>(null);
+
+  const setItem = (key: string, item: string) => {
+    localStorage.setItem(key, item);
+    setValue(item);
+  };
+
+  const getItem = (key: string) => {
+    const item = localStorage.getItem(key);
+    setValue(item);
+    return item;
+  };
+
+  const removeItem = (key: string) => {
+    localStorage.removeItem(key);
+    setValue(null);
+  };
+
+  return { value, setItem, getItem, removeItem };
+};
+
+export default useLocalStorage;
