743d1a92238235a17346c1a084ebb5b318fee153
unknown <ahmedirheem2003@gmail.com>
2023-07-01 17:32:26 +0300

    build: useUser context

---
+++ client/src/hooks/useUser.ts
@@ -0,0 +1,30 @@
+import { useContext } from 'react';
+import { AuthContext } from '../context';
+import useLocalStorage from './useLocalStorage';
+
+interface User {
+  id: number;
+  username: string;
+  email: string;
+  avatar: string;
+  authToken?: string;
+}
+
+const useUser = () => {
+  const { user, setUser } = useContext(AuthContext);
+  const { setItem } = useLocalStorage();
+
+  const addUser = (userData: User) => {
+    setUser(userData);
+    setItem('user', JSON.stringify(userData));
+  };
+
+  const removeUser = () => {
+    setUser(null);
+    setItem('user', '');
+  };
+
+  return { user, addUser, removeUser };
+};
+
+export default useUser;
