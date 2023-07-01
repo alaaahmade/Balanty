f78cb68810b3aafbd616bf6228c8eed802e20ca2
unknown <ahmedirheem2003@gmail.com>
2023-07-01 18:04:13 +0300

    refactor: refactor import/export

--- client/src/context/index.ts
+++ client/src/context/index.ts
@@ -1,2 +1,5 @@
+import AuthContext from './AuthContext';
+
 export { open, useCustomOpen } from './RootLayout';
 export { statsContext, StatsContextProvider } from './CreateMatch';
+export { AuthContext };
