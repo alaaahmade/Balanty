b10f8acc7f84fb9cb171197d3f105fe178baf94b
unknown <ahmedirheem2003@gmail.com>
2023-07-03 16:44:01 +0300

    fix: fix login schema

--- server/validations/authSchema.ts
+++ server/validations/authSchema.ts
@@ -13,7 +13,7 @@ const signupSchema = Joi.object({
 });
 
 const loginSchema = Joi.object({
-  username: Joi.string().min(4).max(25).required().messages({
+  username: Joi.string().min(2).max(25).required().messages({
     'string.empty': 'هذا الحقل لا يمكن أن يكون فارغ',
     'string.min': 'يجب ألا يقل هذا الحقل عن 5 أحرف',
     'string.max': 'يجب ألا يزيد هذا الحقل عن 25 حرفًا',
