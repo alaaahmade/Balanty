904061c357da4066b7e0c9a0f65553c0e29dbc70
Emanalabsi <emanabsi30@gmail.com>
2023-07-03 16:16:29 +0300

    fix: make separate components for card and button

--- client/src/components/playerProfile/PlayerFollows.tsx
+++ client/src/components/playerProfile/PlayerFollows.tsx
@@ -1,44 +1,14 @@
 import React from 'react';
-import { Avatar, Button, Typography, Card } from '@mui/material';
+import { Avatar, Typography } from '@mui/material';
+import { FollowCard, FollowButton } from './Player.Styled';
 
 const PlayerFollows: React.FC<{ action: string; username: string }> = ({
   action,
   username,
 }) => {
   return (
-    <Card
-      sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        gap: '12px',
-        height: '100px',
-        marginTop: '20px',
-        border: 'none',
-        boxShadow:
-          '0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 1px rgb(222 222 222), 0px 1px 3px 0px #2cb67400',
-        padding: '0 2rem',
-        borderTop: '0.1px solid #0000002b',
-        width: '500px',
-      }}
-    >
-      <Button
-        sx={{
-          outline: 0,
-          background: '#2CB674',
-          borderRadius: '5px',
-          cursor: 'pointer',
-          border: '1px solid transparent',
-          color: '#fff',
-          '&:hover': {
-            backgroundColor: 'transparent',
-            color: '#2CB674',
-            borderColor: '#2CB674',
-          },
-        }}
-      >
-        {action}
-      </Button>
+    <FollowCard>
+      <FollowButton>{action}</FollowButton>
       <Typography sx={{ flexGrow: '2', direction: 'rtl' }} gutterBottom>
         {username}
       </Typography>
@@ -53,7 +23,7 @@ const PlayerFollows: React.FC<{ action: string; username: string }> = ({
           objectFit: 'cover',
         }}
       />
-    </Card>
+    </FollowCard>
   );
 };
 
