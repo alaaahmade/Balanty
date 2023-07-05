07f1e58add5033bd925415affae87858223d26e1
unknown <ahmedirheem2003@gmail.com>
2023-07-05 22:29:17 +0300

    fix: update User model

--- server/models/User.ts
+++ server/models/User.ts
@@ -10,8 +10,8 @@ class User extends Model {
 }
 User.init(
   {
-    username: { type: DataTypes.STRING(50), allowNull: false, unique: true },
-    phone: { type: DataTypes.STRING(50), allowNull: false, unique: true },
+    username: { type: DataTypes.STRING(25), allowNull: false, unique: true },
+    phone: { type: DataTypes.STRING(10), allowNull: false, unique: true },
     email: { type: DataTypes.STRING, allowNull: false },
     password: { type: DataTypes.STRING, allowNull: false },
     role: { type: DataTypes.STRING, allowNull: false },
