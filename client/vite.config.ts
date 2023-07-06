17f2024db1bb94a5d14bb072800bcd1069f4382c
alaaahmade <alaaahmade66@gmail.com>
2023-07-07 01:24:21 +0300

    build: deploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // Replace with your backend server URL
+        target: 'http://54.93.229.206', // Replace with your backend server URL
         changeOrigin: true,
         // rewrite: path => path.replace(/^\/api/, ''),
       },
