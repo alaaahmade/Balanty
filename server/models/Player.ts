1e73efb3c76dbc8605542d3e7f9ac24a7dce4863
Emanalabsi <emanabsi30@gmail.com>
2023-07-18 20:31:23 +0300

    fix: add default values for avatar and bio

--- server/models/Player.ts
+++ server/models/Player.ts
@@ -12,13 +12,21 @@ class Player extends Model {
 
 Player.init(
   {
-    avatar: { type: DataTypes.TEXT },
-    age: { type: DataTypes.INTEGER },
+    avatar: {
+      type: DataTypes.TEXT,
+      defaultValue:
+        'https://www.yourstru.ly/wp-content/uploads/2022/12/2022-12-22_12_lionel-messi-biograph.webp',
+    },
+    age: { type: DataTypes.INTEGER, defaultValue: 'لم يتم ادخال عمر' },
     position: {
       type: DataTypes.STRING(100),
       defaultValue: 'لم يتم ادخال مركز',
     },
-    cover: { type: DataTypes.TEXT },
+    cover: {
+      type: DataTypes.TEXT,
+      defaultValue:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz-EbTsSBS9XCU_ten8O9UB1m9q4UwXxsZ_A&usqp=CAU',
+    },
     bio: { type: DataTypes.TEXT, defaultValue: 'لم يتم ادخال وصف' },
   },
   {
