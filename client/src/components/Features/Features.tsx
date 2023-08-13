4a8ad9fed346bf9f469bcaebe40cec16ddce6925
unknown <ahmedirheem2003@gmail.com>
2023-08-13 16:45:29 +0300

    fix: add images

--- client/src/components/Features/Features.tsx
+++ client/src/components/Features/Features.tsx
@@ -9,15 +9,15 @@ const Feature: React.FC = (): ReactElement => {
       <StyledTitleF>احجز, العب, شارك, شوت</StyledTitleF>
       <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
         <FeatureSection
-          imageSrc="https://res.cloudinary.com/dtpbcx2kv/image/upload/v1688465399/hbn3hcjtk4yiovgy58pi.svg"
+          imageSrc="https://res.cloudinary.com/dtpbcx2kv/image/upload/v1691933957/tgs8tejae3iiokkgnmag.png"
           description=" كرة القدم هي رياضة شعبية ومحبوبة حول العالم. تتميز اللعبة بجماعيتها وتطلب التعاون والتنسيق بين اللاعبين وهي وسيلة للترفيه والتنافس وبناء الروح الرياضية."
         />
         <FeatureSection
-          imageSrc="https://res.cloudinary.com/dtpbcx2kv/image/upload/v1688464333/hue1yv1mb5ejro9uxawd.svg"
+          imageSrc="https://res.cloudinary.com/dtpbcx2kv/image/upload/v1691934010/wldppy9zexq4emsz4j7f.png"
           description="تعتبر الكرة القدم أيضًا رياضة ذات تاريخ طويل وتطور مستمر. تشمل البطولات العالمية مثل كأس العالم والبطولات القارية مثل دوري أبطال أوروبا و أفريقيا والعديد من الدوريات التي تجمع المشجعين واللاعبين من مختلف الثقافات والبلدان."
         />
         <FeatureSection
-          imageSrc="https://res.cloudinary.com/dtpbcx2kv/image/upload/v1688465311/mrnd7c4gh8pgcwtigkiq.svg"
+          imageSrc="https://res.cloudinary.com/dtpbcx2kv/image/upload/v1691934045/zqfqmeqkdmfbffxnhfi2.png"
           description="تقدم الكرة القدم فوائد صحية وبدنية. تساهم المباريات والتمارين المنتظمة في تحسين اللياقة البدنية والقوة والتحمل. تعزز الكرة القدم أيضًا التواصل والتعاون بين اللاعبين، مما يعزز الروح الجماعية والعمل الجماعي."
         />
       </Box>
